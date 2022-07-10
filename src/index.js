043df7d436d26ae9d507a29e4b18487d19652afc
ivaylotsachev <itsa4ev@gmail.com>
2022-07-10 18:41:19 +0300

    integrate sass

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 // components
 import App from "./App";
 
+// global styles
+import "./styles/styles.scss";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
--- src/router/index.js
+++
@@ -1,4 +0,0 @@
-import createRouter from "react-router-dom";
-import routes from "routes";
-
-export default createRouter();
